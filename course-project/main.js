c9622533e20a591182047803701b0d0503b981df
xenostar <bryan.a.leewood@gmail.com>
2021-11-26 00:21:00 -0800

    main Formatting

--- course-project/main.js
+++ course-project/main.js
@@ -1,34 +1,34 @@
 // Modules
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
 // Create a new BrowserWindow when `app` is ready
-function createWindow () {
+function createWindow() {
+	mainWindow = new BrowserWindow({
+		width: 1000,
+		height: 800,
+		webPreferences: {
+			// --- !! IMPORTANT !! ---
+			// Disable 'contextIsolation' to allow 'nodeIntegration'
+			// 'contextIsolation' defaults to "true" as from Electron v12
+			contextIsolation: false,
+			nodeIntegration: true,
+		},
+	})
 
-  mainWindow = new BrowserWindow({
-    width: 1000, height: 800,
-    webPreferences: {
-      // --- !! IMPORTANT !! ---
-      // Disable 'contextIsolation' to allow 'nodeIntegration'
-      // 'contextIsolation' defaults to "true" as from Electron v12
-      contextIsolation: false,
-      nodeIntegration: true
-    }
-  })
+	// Load main.html into the new BrowserWindow
+	mainWindow.loadFile('renderer/main.html')
 
-  // Load main.html into the new BrowserWindow
-  mainWindow.loadFile('renderer/main.html')
+	// Open DevTools - Remove for PRODUCTION!
+	mainWindow.webContents.openDevTools()
 
-  // Open DevTools - Remove for PRODUCTION!
-  mainWindow.webContents.openDevTools();
-
-  // Listen for window being closed
-  mainWindow.on('closed',  () => {
-    mainWindow = null
-  })
+	// Listen for window being closed
+	mainWindow.on('closed', () => {
+		mainWindow = null
+	})
 }
 
 // Electron `app` is ready
@@ -36,10 +36,10 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed - (Not macOS - Darwin)
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
+	if (process.platform !== 'darwin') app.quit()
 })
 
 // When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
 app.on('activate', () => {
-  if (mainWindow === null) createWindow()
+	if (mainWindow === null) createWindow()
 })
