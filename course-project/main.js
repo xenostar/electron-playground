11260522c7717e3577136c4b0f2aa2acbb083b94
xenostar <bryan.a.leewood@gmail.com>
2021-11-28 02:21:23 -0800

    main Build stuff.

--- course-project/main.js
+++ course-project/main.js
@@ -48,7 +48,7 @@ function createWindow () {
   state.manage(mainWindow)
 
   // Open DevTools - Remove for PRODUCTION!
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   // Listen for window being closed
   mainWindow.on('closed',  () => {
