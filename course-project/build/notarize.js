4c12c7c6ce3cd9486d22fe180f0ff8085e92c1e6
xenostar <bryan.a.leewood@gmail.com>
2021-11-28 22:56:34 -0800

    main Adding notarize stuff.

--- course-project/build/notarize.js
+++ course-project/build/notarize.js
@@ -1,23 +1,18 @@
+require('dotenv').config()
 const { notarize } = require('electron-notarize')
 
-console.log('Notarizing app')
+exports.default = async function notarizing(context) {
+	const { electronPlatformName, appOutDir } = context
 
-// Default module export
-exports.default = async function (context) {
-	// Skip notarize if not MacOS
-	if (process.platform !== 'darwin') {
+	if (electronPlatformName !== 'darwin') {
 		return
 	}
 
-	// Get some context vars
-	let appName = context.packager.appInfo.productFilename
-	let appDir = context.appOutDir
+	const appName = context.packager.appInfo.productFilename
 
-	// Run Notarize
 	return await notarize({
-		// Must match appId in package.json. Can also include package.json and read it here
 		appBundleId: 'com.xenostar.readit',
-		appPath: `${appDir}/${appName}.app`,
+		appPath: `${appOutDir}/${appName}.app`,
 		appleId: process.env.APPLE_ID,
 		appleIdPassword: process.env.APPLE_ID_PASSWORD,
 	})
