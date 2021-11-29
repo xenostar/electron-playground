131ea843f5d699cff11cd9f2803f1210ca2c5958
xenostar <bryan.a.leewood@gmail.com>
2021-11-28 23:16:19 -0800

    main More verbose version of notarize

--- course-project/build/notarize.js
+++ course-project/build/notarize.js
@@ -1,19 +1,44 @@
 require('dotenv').config()
 const { notarize } = require('electron-notarize')
+const fs = require('fs')
+const path = require('path')
 
 exports.default = async function notarizing(context) {
+	console.log('electron-builder hook: afterSign')
 	const { electronPlatformName, appOutDir } = context
 
 	if (electronPlatformName !== 'darwin') {
+		console.log('Skipping notarization because this build is not MacOS...')
 		return
 	}
 
-	const appName = context.packager.appInfo.productFilename
+	// Same as appId in package.json
+	const appBundleId = 'com.xenostar.readit'
+	const appPath = path.join(appOutDir, `${context.packager.appInfo.productFilename}.app`)
 
-	return await notarize({
-		appBundleId: 'com.xenostar.readit',
-		appPath: `${appOutDir}/${appName}.app`,
-		appleId: process.env.APPLE_ID,
-		appleIdPassword: process.env.APPLE_ID_PASSWORD,
-	})
+	// Check if app exists
+	if (!fs.existsSync(appPath)) {
+		throw new Error(`Cannot find application at: ${appPath}`)
+	}
+
+	console.log(
+		`Sending Apple the signed build, for them to notarize: ${appBundleId} found at ${appPath}`
+	)
+
+	// Attempt notarization
+	try {
+		await notarize({
+			appBundleId,
+			appPath,
+			appleId: process.env.APPLE_ID,
+			appleIdPassword: process.env.APPLE_ID_PASSWORD,
+		})
+		console.log('Received notarization ticket from Apple')
+		console.log('Stapled ticket to the App')
+	} catch (error) {
+		console.log('App failed to notarize with error:')
+		console.error(error)
+	}
+
+	console.log(`Done notarizing ${appBundleId}`)
 }
